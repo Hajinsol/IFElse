f1e2b5d456448e18b01cc43f3f502022a9b842d3
Hajinsol <jinsor5542@gmail.com>
2014-10-02 16:57:32 +0900

    8

--- sketch.js
+++ sketch.js
@@ -15,32 +15,32 @@ fill(255, 100, 124);
 
 if (mouseX < 100) {
   fill(255, 100, 124);
-  stroke(255, 100, 124);
+  stroke(255, 200, 124);
   ellipse(100, 200, 100, 100);
 } 
 else if (mouseX < 200){
   fill(12, 192, 147);
-  stroke(12, 192, 147);
+  stroke(255, 200, 124);
   ellipse(150, 200, 200, 200);
 } 
 else if (mouseX < 300){
   fill(211, 31, 56);
-  stroke(211, 31, 56);
+  stroke(255, 200, 124);
   ellipse(250, 200, 300, 300);
 }
  else if (mouseX < 400){
   fill(40, 58, 134);
-  stroke(40, 58, 134);
+  stroke(255, 200, 124);
   ellipse(350, 200, 400, 400);
 }
  else if (mouseX < 500){
   fill(255, 234, 0);
-  stroke(255, 234, 0);
+  stroke(255, 200, 124);
   ellipse(450, 200, 500, 500);
 } 
 else {
    fill(132, 41, 144);
-  stroke(132, 41, 144);
+  stroke(255, 200, 124);
   ellipse(550, 200, 600, 600);
 }
 }
