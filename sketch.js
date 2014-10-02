735ca22af836f0ecd460c9f7159d34cead7a3110
Hajinsol <jinsor5542@gmail.com>
2014-10-02 16:44:48 +0900

    7

--- sketch.js
+++ sketch.js
@@ -5,26 +5,42 @@ function setup() {
   stroke(255, 100, 124);
 }
 
-  function draw()
-  {
+  function draw() {
   background(0,100,124)
-
+fill(255, 100, 124);
     //ellipse(mouseX, mouseY, 50, 50);
     ellipse(mouseX, mouseY, 100, 100);
   
 
 
 if (mouseX < 100) {
-  ellipse(50, 200, 100, 100);
-} else if (mouseX < 200){
-  ellipse(150, 200, 100, 100);
-} else if (mouseX < 300){
-  ellipse(250, 200, 100, 100);
-} else if (mouseX < 400){
-  ellipse(350, 200, 100, 100);
-} else if (mouseX < 500){
-  ellipse(450, 200, 100, 100);
-} else {
-  ellipse(550, 200, 100, 100);
+  fill(255, 100, 124);
+  stroke(255, 100, 124);
+  ellipse(100, 200, 100, 100);
+} 
+else if (mouseX < 200){
+  fill(12, 192, 147);
+  stroke(12, 192, 147);
+  ellipse(150, 200, 200, 200);
+} 
+else if (mouseX < 300){
+  fill(211, 31, 56);
+  stroke(211, 31, 56);
+  ellipse(250, 200, 300, 300);
+}
+ else if (mouseX < 400){
+  fill(40, 58, 134);
+  stroke(40, 58, 134);
+  ellipse(350, 200, 400, 400);
+}
+ else if (mouseX < 500){
+  fill(255, 234, 0);
+  stroke(255, 234, 0);
+  ellipse(450, 200, 500, 500);
+} 
+else {
+   fill(132, 41, 144);
+  stroke(132, 41, 144);
+  ellipse(550, 200, 600, 600);
 }
 }
